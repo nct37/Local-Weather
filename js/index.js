200ba5df7665e1bc308387d1b90c0b6f793fdfdf
Nick <tannernc.nt@gmail.com>
2017-03-22 22:35:22 -0400

    Testing/modifying responsive screen

--- js/index.js
+++ js/index.js
@@ -13,19 +13,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         //Possible refactor to address browser geo location requirement of https:// connection. Codepen doesn't automatically load in https://---connect via a secondary, albeit a less accurate source if user doesn't actively view in https:// or if user denies location request.
 
         //var url = "https://api.apixu.com/v1/current.json?key=e265c4ee836f4d1ca6132647172001&q=" + lat + " " + lon;
-        //function loadData(url, dataFunction) {} 
+        //function loadData(url, dataFunction) {}
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 var response = JSON.parse(xhttp.responseText);
-              
+
                 (function removeLoader() {
-                  
+
                     document.getElementById("loader").style.display = "none";
                 })();
                 //Add date and time
                 document.getElementById("date").innerHTML = date;
-              
+
                 time();
 
                 getData(response);
@@ -84,37 +84,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 var imageUrl = "('https://res.cloudinary.com/dkdgt4co6/image/upload/";
                 var setBackground = condition.toLowerCase();
                 //Set background image based on conditions
-                 if (["sunny", "sunshine", "clear"].indexOf(setBackground) >= 0) {
-                  if (hours >= 21) {
-                    imagePath = "v1489707725/Starry-Night-Sky_arnoyo.jpg";
-                  } else {
-                    imagePath = "c_crop,h_1080,q_94,x_0,y_0/v1489626233/dream_landscape-1920x1080_pq9zx0.jpg";
-                  }
+                if (["sunny", "sunshine", "clear"].indexOf(setBackground) >= 0) {
+                    if (hours >= 21) {
+                        imagePath = "v1489707725/Starry-Night-Sky_arnoyo.jpg";
+                    } else {
+                        imagePath = "c_crop,h_1080,q_94,x_0,y_0/v1489626233/dream_landscape-1920x1080_pq9zx0.jpg";
+                    }
 
                 } else if (["cloudy", "clouds", "overcast", "mostly cloudy"].indexOf(setBackground) >= 0) {
-                  if (hours >= 21) {
-                    imagePath = "v1490145601/211605391_782caa152f_o_psvlkd.jpg";
-                  } else {
-                    imagePath = "v1489628714/England-scenery-fields-tree-cloudy-sky_1920x1200_zjihba.jpg";
-                  }
+                    if (hours >= 21) {
+                        imagePath = "v1490145601/211605391_782caa152f_o_psvlkd.jpg";
+                    } else {
+                        imagePath = "v1489628714/England-scenery-fields-tree-cloudy-sky_1920x1200_zjihba.jpg";
+                    }
 
                 } else if (["partly cloudy", "partly sunny"].indexOf(setBackground) >= 0) {
-                  if (hours >= 21) {
-                    imagePath = "v1490145601/211605391_782caa152f_o_psvlkd.jpg";
-                  } else {
-                    imagePath = "v1490019539/ahoSK4_nd4vsz.jpg";
-                  }
+                    if (hours >= 21) {
+                        imagePath = "v1490145601/211605391_782caa152f_o_psvlkd.jpg";
+                    } else {
+                        imagePath = "v1490019539/ahoSK4_nd4vsz.jpg";
+                    }
 
                 } else if (["rain", "rainy", "showers", "light rain", "heavy rain"].indexOf(setBackground) >= 0) {
                     imagePath = "v1489789747/-_Heavy_rain_Dullness_Bad_weather_Wallpaper_Background_Ultra_HD_4K_phialf.jpg";
 
                 } else if (["storms", "stormy", "thunderstorms", "thunder", "lightning"].indexOf(setBackground) >= 0) {
-                  if (hours >= 21) {
-                    imagePath = "v1489707239/fantastic-lightning-wallpaper-1942-2092-hd-wallpapers_teflue.jpg";
-                  } else {
-                    imagePath = "v1490146875/39544132-free-thunderstorm-wallpapers_un1zrl.jpg";
-                  }
-                    
+                    if (hours >= 21) {
+                        imagePath = "v1489707239/fantastic-lightning-wallpaper-1942-2092-hd-wallpapers_teflue.jpg";
+                    } else {
+                        imagePath = "v1490146875/39544132-free-thunderstorm-wallpapers_un1zrl.jpg";
+                    }
+
                 } else if (["snow", "snowy", "snow storms", "ice"].indexOf(setBackground) >= 0) {
                     imagePath = "v1489707398/nature-seasons-winter-snow-wallpapers-1920x1200_tktbyd.jpg";
                 } else if (["mist", "fog", "patchy fog"].indexOf(setBackground) >= 0) {
